1e8eac4715c773bd6a16cb785805341b6c567366
yucong <yucong1118@gmail.com>
2012-12-14 13:15:05 +0800

    bug fix

--- proxy.js
+++ proxy.js
@@ -1,9 +1,17 @@
 var http = require('http');
 
-var proxyRequest=function(option,callback){
-	http.get(option, function(res) {
+var proxyRequest=function(options,callback){
+	http.get(options, function(res) {
+		var html='';
 		res.on("data", function(chunk) {
-			callback(chunk.toString())
+			html+=chunk;
+		}).on("end", function() {
+			//console.log(options.host+options.path+'-'+res.statusCode);
+			if(res.statusCode==200){
+				callback(html);
+			}else{
+				callback('');
+			}	
 		});
 	}).on('error', function(e) {
 		console.log("Got error: " + e.message);
